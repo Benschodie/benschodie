0c15f66a6e50e2950f63b1fc82d7676f9a9f418c
Benschodie <benjamin.konopka@icloud.com>
2018-09-08 15:50:37 +0200

    Updates

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// Select DOM Items
+const menuBtn = document.querySelector('.menu__btn');
+const menuMain = document.querySelector('.menu__main');
+const menuNav = document.querySelector('.menu__nav');
+const menuBranding = document.querySelector('.menu__branding');
+const navItem = document.querySelectorAll('.menu__navItem');
+
+// Set Initial State of Menu
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    // adding the class of close to menu__btn (its vanilla)
+    menuBtn.classList.add('close');
+    menuMain.classList.add('show');
+    menuNav.classList.add('show');
+    menuBranding.classList.add('show');
+    navItem.forEach(item => item.classList.add('show'));
+
+    // set Menu State
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove('close');
+    menuMain.classList.remove('show');
+    menuNav.classList.remove('show');
+    menuBranding.classList.remove('show');
+    navItem.forEach(item => item.classList.remove('show'));
+
+    // set Menu State
+    showMenu = false;
+  }
+}
+
+// console.log(menuBtn);
