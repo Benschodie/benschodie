c2f2aae7dae7dc72ee83f552d9f6bf5f15475db3
Benschodie <benjamin.konopka@icloud.com>
2018-09-28 17:12:46 +0200

    Updates

--- js/main.js
+++ js/main.js
@@ -67,7 +67,7 @@ TypeWriter.prototype.type = function() {
   this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
 
   // Initial Type Speed
-  let typeSpeed = 300;
+  let typeSpeed = 200;
 
   if (this.isDeleting) {
     typeSpeed /= 2;
@@ -84,7 +84,7 @@ TypeWriter.prototype.type = function() {
     // Move to the next word
     this.wordIndex++;
     // Pause before start typing
-    typeSpeed = 500;
+    typeSpeed = 400;
   }
 
   setTimeout(() => this.type(), typeSpeed);
